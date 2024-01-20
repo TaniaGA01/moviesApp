49d73219240c535c955fdc12ecc27066971019b2
TaniaGA01 <tanygaitan@gmail.com>
2024-01-20 16:05:21 +0100

    refacto getAllPages function

--- src/services/composables/usePagination.ts
+++ src/services/composables/usePagination.ts
@@ -1,5 +1,5 @@
 import { ref } from "vue"
-import type { Movie, Genre } from '@/services/interfaces/movies.interfaces';
+import type { Movie, Genre, MoviesData } from '@/services/interfaces/movies.interfaces';
 import { options } from '@/api/moviesAPI';
 import axios from "axios";
 
@@ -21,35 +21,37 @@ export default function usePagination() {
   const getAllPages = async (pageNumber:number = page.value, titleValue = title.value, yearValue = year.value) => {
     load.value = true
 
-    let url:string = ''
+    const url=ref<string>()
 
     const getData = () => {
       if(titleValue === undefined || yearValue === undefined){
-        url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&region=FR&sort_by=popularity.desc&page=${pageNumber}`
+        url.value = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&region=FR&sort_by=popularity.desc&page=${pageNumber}`
       }
       else{
-        url = `https://api.themoviedb.org/3/search/movie?query=${titleValue}&include_adult=false&language=fr-FR&page=${pageNumber}&year=${yearValue}`
+        url.value = `https://api.themoviedb.org/3/search/movie?query=${titleValue}&include_adult=false&language=fr-FR&page=${pageNumber}&year=${yearValue}`
       }
     }
     getData()
 
+    const data = ref<MoviesData>()
+
     try {
-
-      const urlMovies = url
-
-      const data = (await axios.get(urlMovies, options)).data
-      totalPages.value = data.total_pages
-      totalResults.value = data.total_results
-      page.value = data.page
-      respData.value = data.results
-
-      if(yearValue === undefined && pageNumber === 1){
-        rating.value = respData.value[0].popularity
-      }
+      data.value = (await axios.get(url.value as string, options)).data
     } catch (error) {
       console.log('error', error)
     } finally{
       load.value = false
+    }
+
+    if (data.value) {
+      totalPages.value = data.value.total_pages
+      totalResults.value = data.value.total_results
+      page.value = data.value.page
+      respData.value = data.value.results
+
+      if(yearValue === undefined && pageNumber === 1){
+        rating.value = respData.value[0].popularity
+      }
     }
   }
   getAllPages(page.value, title.value, year.value)
@@ -91,10 +93,6 @@ export default function usePagination() {
     return Math.ceil(page.value)
   }
 
-  const lastPage = (): number => {
-    return totalPages.value
-  }
-
   return {
       load,
       movies,
@@ -113,8 +111,7 @@ export default function usePagination() {
       backPage,
       goToPage,
       firstPage,
-      middlePages,
-      lastPage
+      middlePages
   };
 
 }
